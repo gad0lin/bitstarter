51a39bf5b422697fe2d192e2e292eecb6c985974
kbarczyn <krzysztof.barczynski@roclets.net>
2013-07-14 22:39:28 -0700

    Reading from index.html

--- web.js
+++ web.js
@@ -1,10 +1,14 @@
+fs = require('fs'); 
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+  content = fs.readFileSync("index.html", "utf8")
+  response.send(content);
+  });
+}
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
